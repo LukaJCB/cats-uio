455c4bfca0850811f39368be7e108a17292d9bd4
Luka Jacobowitz <luka.jacobowitz@codecentric.de>
2018-03-23 08:10:35 -0400

    Add Parallel

--- core/src/main/scala/cats/effect/uio/UIO.scala
+++ core/src/main/scala/cats/effect/uio/UIO.scala
@@ -1,6 +1,6 @@
 package cats.effect.uio
 
-import cats.{Monad, MonadError, Monoid, Semigroup}
+import cats.{Applicative, Monad, MonadError, Monoid, Parallel, Semigroup, ~>}
 import cats.effect.IO
 
 
@@ -14,13 +14,11 @@ object UIO extends UIOInstances with Newtype {
   private[cats] def create[A](s: IO[A]): Type[A] =
     s.asInstanceOf[Type[A]]
 
-  private[cats] def unwrap[A](s: Type[A]): IO[A] =
-    s.asInstanceOf[IO[A]]
-
   def fromIO[A](ioa: IO[A]): UIO[Either[Throwable, A]] =
     create(ioa.attempt)
 
-  def runUIO[A](uioa: UIO[A]): IO[A] = unwrap(uioa)
+  def runUIO[A](uioa: UIO[A]): IO[A] =
+    uioa.asInstanceOf[IO[A]]
 
   def runEitherIO[A](uioa: UIO[Either[Throwable, A]]): IO[A] =
     MonadError[IO, Throwable].rethrow(runUIO(uioa))
@@ -30,21 +28,55 @@ object UIO extends UIOInstances with Newtype {
   def pure[A](x: A): UIO[A] = create(IO.pure(x))
 }
 
-private[uio] sealed abstract class UIOInstances {
-  implicit def catsEffectMonadForUIO: Monad[UIO] = new Monad[UIO] {
+private[uio] abstract class UIOParallelNewtype {
+
+  type Par[A] = Par.Type[A]
+
+  object Par extends Newtype {
+
+    def fromUIO[A](s: UIO[A]): Type[A] =
+      s.asInstanceOf[Type[A]]
+
+    def toUIO[A](s: Type[A]): UIO[A] =
+      s.asInstanceOf[UIO[A]]
+
+  }
+}
+
+private[uio] sealed abstract class UIOInstances extends UIOParallelNewtype {
+  implicit val catsEffectMonadForUIO: Monad[UIO] = new Monad[UIO] {
     def tailRecM[A, B](a: A)(f: A => UIO[Either[A, B]]): UIO[B] =
-      UIO.create(Monad[IO].tailRecM(a)(f andThen UIO.unwrap))
+      UIO.create(Monad[IO].tailRecM(a)(f andThen UIO.runUIO))
 
     def flatMap[A, B](fa: UIO[A])(f: A => UIO[B]): UIO[B] =
-      UIO.create(Monad[IO].flatMap(UIO.unwrap(fa))(f andThen UIO.unwrap))
+      UIO.create(Monad[IO].flatMap(UIO.runUIO(fa))(f andThen UIO.runUIO))
 
     def pure[A](x: A): UIO[A] =
       UIO.pure(x)
   }
 
+  implicit val catsEffectApplicativeForParUIO: Applicative[UIO.Par] = new Applicative[UIO.Par] {
+    def pure[A](x: A): UIO.Par[A] = UIO.Par.fromUIO(UIO.pure(x))
+
+    def ap[A, B](ff: UIO.Par[A => B])(fa: UIO.Par[A]): UIO.Par[B] =
+      UIO.Par.fromUIO(UIO.create(Parallel.parAp(UIO.runUIO(UIO.Par.toUIO(ff)))(UIO.runUIO(UIO.Par.toUIO(fa)))))
+  }
+
+  implicit val catsEffectParallelForUIO: Parallel[UIO, UIO.Par] = new Parallel[UIO, UIO.Par] {
+    def applicative: Applicative[UIO.Par] = catsEffectApplicativeForParUIO
+
+    def monad: Monad[UIO] = catsEffectMonadForUIO
+
+    def sequential: ~>[UIO.Par, UIO] =
+      new ~>[UIO.Par, UIO] { def apply[A](fa: UIO.Par[A]): UIO[A] = UIO.Par.toUIO(fa) }
+
+    def parallel: ~>[UIO, UIO.Par] =
+      new ~>[UIO, UIO.Par] { def apply[A](fa: UIO[A]): UIO.Par[A] = UIO.Par.fromUIO(fa) }
+  }
+
   implicit def catsEffectMonoidForUIO[A: Monoid]: Monoid[UIO[A]] = new Monoid[UIO[A]] {
     def empty: UIO[A] = UIO.pure(Monoid[A].empty)
     def combine(x: UIO[A], y: UIO[A]): UIO[A] =
-      UIO.create(UIO.unwrap(x).flatMap(a1 => UIO.unwrap(y).map(a2 => Semigroup[A].combine(a1, a2))))
+      UIO.create(UIO.runUIO(x).flatMap(a1 => UIO.runUIO(y).map(a2 => Semigroup[A].combine(a1, a2))))
   }
 }
---
+++ tests/src/test/scala/cats/effect/uio/tests/UIOTestSuite.scala
@@ -0,0 +1,35 @@
+package cats.effect.uio
+package tests
+
+import cats.Eq
+import cats.effect.laws.util.TestContext
+import cats.tests.CatsSuite
+import cats.laws.discipline.{ApplicativeTests, MonadTests, ParallelTests}
+import org.scalacheck.{Arbitrary, Cogen}
+import cats.effect.laws.util.TestInstances
+import cats.effect.laws.discipline.arbitrary.catsEffectLawsArbitraryForIO
+import cats.kernel.laws.discipline.MonoidTests
+
+class UIOTestSuite extends CatsSuite {
+
+  implicit val testContext: TestContext = TestContext()
+
+  implicit def eqUIO[A: Eq]: Eq[UIO[A]] =
+    TestInstances.eqIO[A].imap(UIO.create)(uio => UIO.runUIO(uio))
+
+  implicit def eqParUIO[A: Eq]: Eq[UIO.Par[A]] =
+    eqUIO[A].imap(UIO.Par.fromUIO)(UIO.Par.toUIO)
+
+  implicit def arbitraryUIO[A: Arbitrary: Cogen]: Arbitrary[UIO[A]] =
+    Arbitrary(catsEffectLawsArbitraryForIO[A].arbitrary.map(io => UIO.create(io)))
+
+  implicit def arbitraryParUIO[A: Arbitrary: Cogen]: Arbitrary[UIO.Par[A]] =
+    Arbitrary(arbitraryUIO[A].arbitrary.map(uio => UIO.Par.fromUIO(uio)))
+
+  checkAll("Monoid[UIO]", MonoidTests[UIO[String]].monoid)
+
+  checkAll("Monad[UIO]", MonadTests[UIO].monad[Int, String, Int])
+  checkAll("Applicative[UIO.Par]", ApplicativeTests[UIO.Par].applicative[String, Int, Int])
+
+  checkAll("Parallel[UIO, UIO.Par]", ParallelTests[UIO, UIO.Par].parallel[Int, String])
+}
